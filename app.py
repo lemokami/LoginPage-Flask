bfa6ee9b8c2b43b3ddacfa8006daabb92a9d9568
jesswinw <jesswinw@gmail.com>
2020-04-24 15:51:37 +0530

    some minor mods 🤓

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 
 
 class User(db.Model):
