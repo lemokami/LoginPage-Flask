175eec86a519b4f60c3393285c70780cec9adf3e
jesswinw <jesswinw@gmail.com>
2020-04-24 17:46:25 +0530

    Made  a better greet page ♥

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
